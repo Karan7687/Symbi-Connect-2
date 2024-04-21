3d1c00fd96b5ff457eb377b022f1b9db89bdafad
Karan <karan.l.k123321@gmail.com>
2024-04-21 14:49:01 +0530

    Protected Log In Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,21 @@ import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
 import Qna from "./components/qna";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import { NotLoggedInRoutes } from "./routes/NotLoggedInRoutes";
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Login />} exact />
-        <Route path="/profile" element={<Profile />} exact />
-        <Route path="/" element={<Home />} exact />
+        <Route element={<LoggedInRoutes />}>
+          <Route path="/profile" element={<Profile />} exact />
+          <Route path="/" element={<Home />} exact />
+        </Route>
+
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} exact />
+        </Route>
         <Route path="/qna" element={<Qna />} exact />
-        
       </Routes>
     </div>
   );
